ab80db05bac88c7218b72ce9ece4a6d8cab763c0
Yash Mittal <mittal.yash12@hotmail.com>
2022-10-16 00:26:34 +0530

    Feat: service fot on_init
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

--- apps/beckn-bap/src/on_init/on_init.service.ts
+++ apps/beckn-bap/src/on_init/on_init.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { lastValueFrom, map } from 'rxjs';
 import { OnInitDTO } from './dto/on_init.dto';
 
 @Injectable()
 export class OnInitService {
-  create(onInitDto: OnInitDTO) {
-    return 'This action adds a new onInit';
+  constructor(private readonly httpService: HttpService) { }
+  async create(onInitDto: OnInitDTO) {
+    try {
+      const requestOptions = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: onInitDto,
+        redirect: 'follow',
+      };
+
+      const responseData = await lastValueFrom(
+        this.httpService
+          .post('http://localhost:5003/', onInitDto, requestOptions)
+          .pipe(
+            map((response) => {
+              return response.data;
+            }),
+          ),
+      );
+
+      return responseData;
+    } catch (er) {
+      console.log(er);
+      throw new InternalServerErrorException();
+    }
   }
 }
