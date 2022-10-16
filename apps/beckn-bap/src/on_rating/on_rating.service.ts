4e97dd5b0e6a53ac659fb15849ddf22139d67db6
Yash Mittal <mittal.yash12@hotmail.com>
2022-10-16 15:03:07 +0530

    Fix: Fixed BPP URI
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

--- apps/beckn-bap/src/on_rating/on_rating.service.ts
+++ apps/beckn-bap/src/on_rating/on_rating.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { lastValueFrom, map } from 'rxjs';
 import { OnRatingDTO } from './dto/on_rating.dto';
 
 @Injectable()
 export class OnRatingService {
-  create(onRatingDto: OnRatingDTO) {
-    return 'This action adds a new onRating';
+  constructor(private readonly httpService: HttpService) { }
+  async create(onRatingDto: OnRatingDTO) {
+    try {
+      const requestOptions = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: onRatingDto,
+        redirect: 'follow',
+      };
+
+      const responseData = await lastValueFrom(
+        this.httpService
+          .post(onRatingDto.context.bpp_uri, onRatingDto, requestOptions)
+          .pipe(
+            map((response) => {
+              return response.data;
+            }),
+          ),
+      );
+
+      return responseData;
+    } catch (err) {
+      console.log(err);
+      throw new InternalServerErrorException();
+    }
   }
 }
