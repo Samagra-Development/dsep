cffcf4ccdf90114d4cae519efb5320b4a8aa9864
Yash Mittal <mittal.yash12@hotmail.com>
2022-10-16 21:15:42 +0530

    Fix: Fixed on-support module
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

--- apps/beckn-bap/src/on_support/on_support.controller.ts
+++ apps/beckn-bap/src/on_support/on_support.controller.ts
@@ -1,34 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { OnSupportService } from './on_support.service';
-import { CreateOnSupportDto } from './dto/create-on_support.dto';
-import { UpdateOnSupportDto } from './dto/update-on_support.dto';
+import { OnSupportDTO } from './dto/on_support.dto';
 
 @Controller('on-support')
 export class OnSupportController {
-  constructor(private readonly onSupportService: OnSupportService) {}
+  constructor(private readonly onSupportService: OnSupportService) { }
 
   @Post()
-  create(@Body() createOnSupportDto: CreateOnSupportDto) {
-    return this.onSupportService.create(createOnSupportDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.onSupportService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.onSupportService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOnSupportDto: UpdateOnSupportDto) {
-    return this.onSupportService.update(+id, updateOnSupportDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.onSupportService.remove(+id);
+  create(@Body() onSupportDto: OnSupportDTO) {
+    return this.onSupportService.create(onSupportDto);
   }
 }
