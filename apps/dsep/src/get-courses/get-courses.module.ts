c2b7ea2b3965b94b32ce789e37f200db37285619
Yash Mittal <mittal.yash12@hotmail.com>
2022-12-10 12:59:07 +0530

    Refactor: Move DSEP to DSEP-Provider

--- apps/dsep/src/get-courses/get-courses.module.ts
+++
@@ -1,9 +0,0 @@
-import { HttpModule } from '@nestjs/axios';
-import { Module } from '@nestjs/common';
-import { GetCoursesService } from './get-courses.service';
-
-@Module({
-  imports: [HttpModule],
-  providers: [GetCoursesService],
-})
-export class GetCoursesModule { }
