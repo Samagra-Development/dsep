7c310b93da48a6fac0988e1d61776c33658feceb
Ajay Maurya <ajaymaurya6798@gmail.com>
2023-12-14 12:49:04 +0530

    forwarded confirm response for marketplace portal

--- apps/bap/src/on_confirm/on_confirm.service.ts
+++ apps/bap/src/on_confirm/on_confirm.service.ts
@@ -2,12 +2,17 @@ import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { generateResponse } from '../utils/utils';
 import { OnConfirmDTO } from './dto/on_confirm.dto';
+import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
 
 @Injectable()
 export class OnConfirmService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {}
 
-  handleOnConfirm(onConfirmDto: OnConfirmDTO) {
+  async handleOnConfirm(onConfirmDto: OnConfirmDTO) {
     console.log(
       'onConfirmDto-------on_confirm service---------:',
       JSON.stringify(onConfirmDto, null, 1),
@@ -16,6 +21,27 @@ export class OnConfirmService {
 
     console.log('response----------on_confirm service------:', response);
 
+    const marketPlaceServiceUrl = this.configService.get<string>(
+      'MARKETPLACE_PORTAL_SERVICE_URL',
+    );
+
+    let url: string;
+
+    // Check if marketPlaceServiceUrl ends with a forward slash or not
+    if (marketPlaceServiceUrl.endsWith('/')) {
+      url = `${marketPlaceServiceUrl}api/consumer/course/update-purchase/confirm`;
+    } else {
+      url = `${marketPlaceServiceUrl}/api/consumer/course/update-purchase/confirm`;
+    }
+
+    console.log(
+      '\n\n marketplace portal service url on_confirm line- 38 ------------:',
+      url,
+      '\n\n',
+    );
+
+    const { data } = await axios.patch(url, response);
+    console.log('update data response from marketplace portal--------:', data);
     //TODO: add endpoints for marketplace-portal-service/course-manager-service to update the data.
 
     return response;
